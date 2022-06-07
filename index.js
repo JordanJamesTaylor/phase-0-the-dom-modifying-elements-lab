1ff8129928edecfeb8d4e6ff34edbc75b43340d6
jordan <jordjamestaylor@gmail.com>
2022-06-07 11:48:59 -0400

    commit tree d47d181c5ec009ad224b9471fc0a3f5e68feaf16

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+let removeMain = document.getElementById('main')
+removeMain.remove()
+
+newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+document.body.append(newHeader)
+
+newHeader = document.querySelector('h1 #victory')
+
+newHeader = document.getElementById('victory')
+
+newHader.textContent("Jordan is the champion")
